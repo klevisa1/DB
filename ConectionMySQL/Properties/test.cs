2bc45d44a0e8fde0be15907a4bc02c7506b7e2b5
klevisa1 <klevisakollarja@gmail.com>
2020-01-11 16:11:57 +0100

    First Commit

---
+++ ConectionMySQL/Properties/test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ConectionMySQL.Properties
+{
+    public class test
+    {
+        static void Main(string[]args)
+        {
+            DBConnection conn = DBConnection.Instance();
+            conn.DatabaseName = "student";
+            try
+            {
+                if (conn.IsConnect())
+                {
+                    Console.WriteLine("Conectet to Database");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Connection failed");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //conn.Close();
+                Console.WriteLine("Fundi i app");
+            }
+                
+            
+
+
+        }
+        
+    }
+}
